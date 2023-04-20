4dea3175183f49e41b6171f7648fe4081968bd0e
Alexandr Lisimenko <lisimenko.alexander@yandex.ru>
2023-04-20 14:39:32 +0300

    add binary_search.py

---
+++ binary_search.py
@@ -0,0 +1,22 @@
+# Бинарный поиск работает только с отсортированным массивом.
+
+
+def binary_search(list, item):
+    low = 0 # нижняя граница массива для поиска 
+    high = len(list) - 1 # верхняя граница списка для поиска
+    while low <= high: # пока эта не сократиться до одного элемента...
+        mid = int((low + high) / 2) # ...проверяем средний элемент
+        guess = list[mid]
+        if guess == item: # значение найдено
+            return mid
+        if guess > item: # много
+            high = mid - 1
+        else: # мало
+            low = mid + 1
+    return None # значение не существует
+
+
+my_list = [4, 6, 7, 26, 54, 67, 100, 139, 202, 376]
+
+print(binary_search(my_list, 67))
+print(binary_search(my_list, 533))
